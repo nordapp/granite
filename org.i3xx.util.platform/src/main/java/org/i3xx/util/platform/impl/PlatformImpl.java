ef7b0e31bc2c0517dfa3c782ec42f3af17061ede
Stefan <Stefan.Hauptmann@ids-dialogsysteme.net>
2015-05-04 13:47:39 +0200

    Initial import of existing project

---
+++ org.i3xx.util.platform/src/main/java/org/i3xx/util/platform/impl/PlatformImpl.java
@@ -0,0 +1,47 @@
+package org.i3xx.util.platform.impl;
+
+/*
+ * #%L
+ * NordApp OfficeBase :: util :: platform
+ * %%
+ * Copyright (C) 2014 - 2015 I.D.S. DialogSysteme GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import java.util.NoSuchElementException;
+
+import org.i3xx.util.basic.platform.Platform;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+public class PlatformImpl {
+	
+	/**
+	 * Gets the current installed platform service
+	 * 
+	 * @return
+	 */
+	public static final Platform getPlatformService(BundleContext context) {
+		ServiceReference<Platform> psrv = context.getServiceReference(Platform.class);
+		if(psrv==null)
+			throw new NoSuchElementException("The requested service reference 'Platform' is not available.");
+		Platform plaf = context.getService(psrv);
+		if(plaf==null)
+			throw new NoSuchElementException("The requested service 'Platform' is not available.");
+		
+		return plaf;
+	}
+}
---
+++ org.i3xx.util.platform/src/main/java/org/i3xx/util/platform/karaf/impl/KarafImpl.java
@@ -0,0 +1,68 @@
+package org.i3xx.util.platform.karaf.impl;
+
+/*
+ * #%L
+ * NordApp OfficeBase :: util :: platform
+ * %%
+ * Copyright (C) 2014 - 2015 I.D.S. DialogSysteme GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import org.i3xx.util.basic.platform.Platform;
+import org.i3xx.util.platform.impl.AvailableKeys;
+import org.osgi.framework.BundleContext;
+
+public class KarafImpl implements Platform {
+	
+	BundleContext bundleContext;
+	
+	public KarafImpl() {
+		bundleContext = null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.i3xx.util.basic.platform.Platform#keys()
+	 */
+	public String[] keys() {
+		return AvailableKeys.getKeys();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.i3xx.util.basic.platform.Platform#getObject(java.lang.String)
+	 */
+	public Object getObject(String key) {
+		if (key.equals( AvailableKeys.SERVER_HOME)) {
+			return bundleContext.getProperty("karaf.home");
+		}
+		else
+			throw new IllegalArgumentException("The key '"+key+"' is not supported yet.");
+	}
+
+	/**
+	 * @return the bundleContext
+	 */
+	public BundleContext getBundleContext() {
+		return bundleContext;
+	}
+
+	/**
+	 * @param bundleContext the bundleContext to set
+	 */
+	public void setBundleContext(BundleContext bundleContext) {
+		this.bundleContext = bundleContext;
+	}
+
+}
