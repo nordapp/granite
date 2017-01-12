68bd87fb2d3306847b75823e40c80966d32e2639
stefan <stefan@DEV6.ids-dialogsysteme.net>
2017-01-12 20:01:38 +0100

    Create Data Access Objects in Hibernate using a generic way

---
+++ org.i3xx.util.basic/src/main/java/org/i3xx/util/basic/core/IGeneratorService.java
@@ -0,0 +1,56 @@
+package org.i3xx.util.basic.core;
+
+/**
+ * The class is used to create new DAO's in a generic way.
+ * 
+ * @author stefan
+ *
+ * @param <E>
+ */
+public interface IGeneratorService<E> {
+
+	/**
+	 * Gets the object by it's guid
+	 * 
+	 * @param guid The guid of the object
+	 * @return The object or null if it isn't available
+	 */
+	E getObject(long guid);
+	
+	/**
+	 * Gets the object by it's uuid
+	 * 
+	 * @param uuid The uuid of the object
+	 * @return The object or null if it isn't available
+	 */
+	E getObject(String uuid);
+	
+	/**
+	 * Gets the object or creates a new object if it isn't available.
+	 * 
+	 * Create a new Object using a generic UUID
+	 * 
+	 * @param uuid The uuid of the object
+	 * @return The object or a new one if it isn't available
+	 */
+	E getOrCreateObject(String uuid);
+	
+	/**
+	 * Gets the object or creates a new object if it isn't available.
+	 * 
+	 * @param uuid The uuid of the object
+	 * @param history The history of the object
+	 * @return The object or a new one if it isn't available
+	 */
+	E getOrCreateObject(String uuid, String history);
+	
+	/**
+	 * Gets the object or creates a new one if it isn't available.
+	 * 
+	 * @param guid The guid of the object
+	 * @param uuid The uuid of the object
+	 * @param history The history of the object
+	 * @return The object or a new one if it isn't available
+	 */
+	E getOrCreateObject(long guid, String uuid, String history);
+}
